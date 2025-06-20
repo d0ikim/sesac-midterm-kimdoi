d782e4af643d975a388bc961671330a75bbba9f9
Doi Kim <doyclub@naver.com>
2025-06-20 14:56:15 +0900

    HTML

---
+++ frontend/js/main.js
@@ -0,0 +1,17 @@
+const emailEl = document.querySelector("#email"); // 이메일 요소
+const passwordEl = document.querySelector("#password"); // 비밀번호 요소
+
+// 입력값이 비어 있으면, 로그인 시도를 막고 오류 메시지를 출력해야 한다.
+// 예시) “모든 항목을 입력해주세요.”
+console.log(emailEl);
+console.log(passwordEl);
+
+
+
+
+
+// 이메일, 비밀번호가 모두 입력되었는지 확인하고, 등록된 사용자 정보와 일치 여부 확인
+
+// 사용자 검증 로직을 작성하시오
+
+// 로그인 성공 시 localStorage에 사용자 정보를 저장하시오.
